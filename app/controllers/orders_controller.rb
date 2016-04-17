aa85932ea89289d36c230a4f5fb128d436f98101
Samantha Diamond <samanthadiamond89@gmail.com>
2016-04-18 07:18:46 +1000

    stripe payment setup but few bugs to fix

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,25 @@
 class OrdersController < ApplicationController
-  def edit
-  end
-
-  def index
-  end
-
   def new
   end
 
-  def show
+  def create
+    # Amount in cents
+    @amount =
+
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+
+    order = Stripe::Order.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'BuddyUp customer',
+      :currency    => 'usd'
+    )
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
   end
 end
