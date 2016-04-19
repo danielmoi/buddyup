0bbaa3c6f59edcd03bd549fae614eb96d110a9b7
Samantha Diamond <samanthadiamond89@gmail.com>
2016-04-19 21:35:51 +1000

    admin functionality added

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,16 +1,40 @@
 class CategoriesController < ApplicationController
-  def edit
-  end
+  before_action :authorise, :only => [:index, :new]
 
   def index
     @categories = Category.all
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      redirect_to categories_path
+    else
+      render 'new'
+    end
   end
 
   def show
     @category = Category.find params[:id]
     @goals = @category.goals
+  end
+
+  def destroy
+    @category = Category.find params[:id]
+    @category.destroy
+    redirect_to categories_path
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:title)
+  end
+
+  def authorise
+  redirect_to root_path unless (@current_user.present? && @current_user.admin?)
   end
 end
