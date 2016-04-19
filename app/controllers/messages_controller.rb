796d3dcc2d561caa59b25e6486c4045b46e668d3
danielmoi <daniel@ampersandmoi.com>
2016-04-19 11:51:34 +1000

    Fix white listing bug by changing form_for syntax

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -29,6 +29,7 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params[goal_messages_path].permit(:initiator_id, :acceptor_id, :subject, :content)
+    params.require(:message).permit(:initiator_id, :acceptor_id, :subject, :content)
+
   end
 end
