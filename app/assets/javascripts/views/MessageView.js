ab80ccec79df5b91cd8ae9c0488976005632b7fe
danielmoi <daniel@ampersandmoi.com>
2016-04-21 10:24:24 +1000

    Render messages successfully

--- app/assets/javascripts/views/MessageView.js
+++ app/assets/javascripts/views/MessageView.js
@@ -2,11 +2,28 @@ var app = app || {};
 
 app.MessageView = Backbone.View.extend({
   tagName: 'div',
+  className: 'message-each__container',
 
   render: function() {
     var subject = this.model.get('subject');
-    console.log('SDFLKJ');
-    this.$el.text( subject );
+    var content = this.model.get('content');
+    var timestamp = this.model.get('created_at');
+
+    $p = $('<p>');
+    $p.text( "Subject: " + subject );
+    $p.addClass('message-each__subject');
+    $p.appendTo(this.$el);
+
+    $p = $('<p>');
+    $p.text( "Content: " + content );
+    $p.addClass('message-each__content');
+    $p.appendTo(this.$el);
+
+    $p = $('<p>');
+    $p.text( "Sent: " + timestamp );
+    $p.addClass('message-each__timestamp');
+    $p.appendTo(this.$el);
+
     this.$el.prependTo('#magic-messages');
   }
 });
